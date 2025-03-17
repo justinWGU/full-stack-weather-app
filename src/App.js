f6e2d9ddd8ae71383c308e5093c16a9f55f64960
justinWGU <Justin_ortiz123@yahoo.com>
2025-03-17 10:25:59 -0700

    Seperated code into InputBox & Weather components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import InputBox from './InputBox.js';
+import Weather from './Weather.js';
 
-function App() {
+
+// const Test = () => {
+//   const [test, setTest] = useState([null]);
+
+//   const handleClick = (event) => {
+//     event.preventDefault();
+
+//     fetch("http://127.0.0.1:8000/core/register/", {method: 'POST'})
+//     .then(response => response.json())
+//     .then(json => setTest(json))
+//     .catch(errors => console.error(errors));
+//   }
+
+//   return (
+//     <div>
+//     <p>Test data: {test.username} {test.password}</p>
+//     <button onClick={handleClick}>Test Button</button>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const [city, setCity] = useState(null);
+  const [isAuth, setAuth] = useState(null);
+
+  // useEffect(() => {
+  //   fetch(url)
+  //   .then(response => response.json())
+  //   .then(json => setData(json))
+  //   .catch(errors => console.error(errors))
+  // }, []);
+
+  const handleChange = (event) => {
+    const newCity = event.target.value;
+    setCity(newCity);
+  }
+
+  const handleSubmit = (event) => {
+    const url = `http://api.weatherapi.com/v1/current.json?key=e77503e6c87a4abf9eb203542242908&q=${city}`;
+
+    event.preventDefault();
+    console.log("Submitted!");
+
+    fetch(url)
+    .then(response => response.json())
+    .then(json => setData(json))
+    .catch(errors => console.error(errors));
+    
+    setCity(null);
+  }
+
+  if (!isAuth) {
+    return (
+      <Authenticate></Authenticate>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <InputBox handleChange={handleChange} handleSubmit={handleSubmit} city={city}></InputBox>
+      <Weather data={data} /> 
+      <Weather data={data} /> 
+      <Weather data={data} />
+    </div>    
   );
 }
 
