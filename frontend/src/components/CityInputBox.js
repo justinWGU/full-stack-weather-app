b537ed7e8588a5a06f9d0d2507530c8fb385bbee
justinWGU <Justin_ortiz123@yahoo.com>
2025-04-10 15:22:21 -0700

    Makes search icon functional

--- frontend/src/components/CityInputBox.js
+++ frontend/src/components/CityInputBox.js
@@ -15,17 +15,17 @@ const [city, setCity] = useState(null);
   }
 
   // Queries city weather data and resets city input box. 
-  const handleSubmit = (event) => {
+  const handleClick = (event) => {
     event.preventDefault();
     setWeatherData(city);
     setCity(null);
   }
 
     return (
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleClick}>
             <div className="bg-gradient-to-r from-blue-600 to-blue-950 px-3 py-3 rounded-xl flex gap-2 justify-end items-center mr-3">
                 <input className="rounded-full py-1 px-1" type="text" value={city || ""} name="cityName" placeholder="Search city..." onChange={handleChange}></input>
-                <img className="" src={search_icon} alt="Search Icon" width="25px"/>
+                <img className="hover:cursor-pointer" src={search_icon} alt="Search Icon" width="25px" onClick={handleClick}/>
             </div>
             </form>
     );
